5e0ee2a8b171d9ab3967a22a89178c47f0964800
icicle4 <icicle4@icloud.com>
2020-04-14 17:51:20 +0800

    init

--- model/pose/nets/hr_pose.py
+++ model/pose/nets/hr_pose.py
@@ -306,7 +306,7 @@ def get_pose_net(cfg, is_train, **kwargs):
 
     model = PoseResNet(block_class, layers, cfg, **kwargs)
 
-    if is_train and cfg.MODEL.INIT_WEIGHTS:
+    if is_train and cfg.get('model', 'init_weights'):
         model.init_weights(cfg.MODEL.PRETRAINED)
 
     return model
